eb5b9806d6bdf6ada30a52f09c9974d4ca73e666
YangSuGuo <3130876713@qq.com>
2024-05-27 21:23:21 +0800

    渐变背景

--- lib/pages/home/views/home.dart
+++ lib/pages/home/views/home.dart
@@ -13,8 +13,37 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
+        body: Stack(
+      children: [
+        // 渐变背景
+        gradientBackground(),
+        Center(
             child: Text(S.of(context).hello_World,
-                style: TextStyle(fontSize: 50.sp))));
+                style: TextStyle(fontSize: 50.sp)))
+      ],
+    ));
   }
+
+  /// 渐变背景
+  Widget gradientBackground() => Align(
+        alignment: Alignment.topLeft,
+        child: Opacity(
+          opacity: 0.6,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [
+                    Theme.of(context).colorScheme.primary.withOpacity(0.9),
+                    Theme.of(context).colorScheme.primary.withOpacity(0.5),
+                    Theme.of(context).colorScheme.surface
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: const [0, 0.0034, 0.34]),
+            ),
+          ),
+        ),
+      );
 }

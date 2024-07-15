aa7aee10b1a4248f168529d8d79948e5228859da
YangSuGuo <3130876713@qq.com>
2024-07-15 18:38:15 +0800

    复制到轻小说

--- lib/pages/tabs/lightNovel/controllers/lightNovel_controller.dart
+++ lib/pages/tabs/lightNovel/controllers/lightNovel_controller.dart
@@ -1,3 +1,53 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:hele_app/http/bangumi_net.dart';
+import 'package:hele_app/model/calendar/calendar.dart';
+import 'package:hele_app/model/search/query_parameters.dart';
+import 'package:hele_app/model/search/request_body.dart';
+import 'package:hele_app/model/search/search.dart';
 
-class LightNovelController extends GetxController {}
+class LightNovelController extends GetxController {
+  final ScrollController scrollController = ScrollController();
+  int limit = 20;
+  int offset = 0;
+  RxList<LegacySubjectSmall> bangumiList = <LegacySubjectSmall>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  // 触底加载
+  Future next() async {
+    offset += 20;
+    await getHotRecommendations();
+  }
+
+  // 获取漫画热门推荐
+  Future getHotRecommendations() async {
+    final QueryParameters queryParameters = QueryParameters(
+      limit: limit,
+      offset: offset,
+    );
+
+    final RequestBody requestBody = RequestBody(
+      filter: Filter(
+        tag: ["轻小说"],
+        type: [1],
+      ),
+      keyword: "",
+      sort: Sort.HEAT,
+    );
+
+    var result = await BangumiNet.getHotRecommendedComics(queryParameters,requestBody);
+    List<Datum>? data = result.data;
+    List<LegacySubjectSmall>? res = data?.map((e) => e.toLegacySubjectSmall()).toList();
+
+    if (offset > 0) {
+      bangumiList.addAll(res ?? []);
+    } else {
+      bangumiList.value = res ?? [];
+    }
+    return res;
+  }
+}
--- lib/pages/tabs/manga/controllers/manga_controller.dart
+++ lib/pages/tabs/manga/controllers/manga_controller.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:hele_app/http/bangumi_net.dart';
 import 'package:hele_app/model/calendar/calendar.dart';
 import 'package:hele_app/model/search/query_parameters.dart';
+import 'package:hele_app/model/search/request_body.dart';
 import 'package:hele_app/model/search/search.dart';
 
 class MangaController extends GetxController {
@@ -18,7 +19,6 @@ class MangaController extends GetxController {
 
   // 触底加载
   Future next() async {
-    print("开始");
     offset += 20;
     await getHotRecommendations();
   }
@@ -30,7 +30,16 @@ class MangaController extends GetxController {
       offset: offset,
     );
 
-    var result = await BangumiNet.getHotRecommendedComics(queryParameters);
+    final RequestBody requestBody = RequestBody(
+      filter: Filter(
+        tag: ["漫画"],
+        type: [1],
+      ),
+      keyword: "",
+      sort: Sort.HEAT,
+    );
+
+    var result = await BangumiNet.getHotRecommendedComics(queryParameters,requestBody);
     List<Datum>? data = result.data;
     List<LegacySubjectSmall>? res = data?.map((e) => e.toLegacySubjectSmall()).toList();
 
